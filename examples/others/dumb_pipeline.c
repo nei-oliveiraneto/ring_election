661668bd66e78101534ac6c8f8e7175f10eed5bb
arthur <eduardo.lorandi@acad.pucrs.br>
2018-05-28 17:31:41 -0300

    Finishing the dumb pipeline, also renaming the file and restructuring the folder tree

--- examples/others/dumb_pipeline.c
+++ examples/others/dumb_pipeline.c
@@ -21,32 +21,35 @@ main(int argc, char** argv)
     
     if (my_rank == 0)
     {
-    //double t1,t2;
-    //t1 = MPI_Wtime();  // inicia a contagem do tempo
-    message[0] = 1;
-    message[1] = 2;
-    message[2] = 3;
-    message[3] = 4;
-    message[4] = 5;
+        double t1,t2;
+        t1 = MPI_Wtime();  // inicia a contagem do tempo
+        message[0] = 10;
+        message[1] = 20;
+        message[2] = 30;
+        message[3] = 40;
+        message[4] = 50;
 
-    int i;
-    for( i=0; i < 5; i++ )
-    {
+        int i;
+        for( i=0; i < 5; i++ )
+        {
             printf("Process %d is sending the %d item\n", my_rank, i);
             MPI_Send( &message[i], 1, MPI_INT, my_rank+1, tag, MPI_COMM_WORLD);
             MPI_Recv( &received[i],1, MPI_INT, proc_n-1, tag, MPI_COMM_WORLD, &status );
-            printf("Process received the message from the last: \n");
-    }
+            printf("Process received the message from the last: %d \n", received[i]);
+        }
 
-    printf( "Finished sending everything" );
-    //t2 = MPI_Wtime(); // termina a contagem do tempo
-    //printf("\nTempo de execucao: %f\n\n", t2-t1);
+        printf( "Finished sending everything\n" );
+        t2 = MPI_Wtime(); // termina a contagem do tempo
+        printf("\nTempo de execucao: %f\n\n", t2-t1);
     }
     else
     {
-        MPI_Recv( &fuck, 1, MPI_INT, my_rank-1, tag, MPI_COMM_WORLD, &status );
-        printf("Repassing\n");
-        MPI_Send( &fuck, 1, MPI_INT, (my_rank+1)%proc_n, tag, MPI_COMM_WORLD);
+        for( int i = 0; i<5; i++ )
+        {
+            MPI_Recv( &fuck, 1, MPI_INT, my_rank-1, tag, MPI_COMM_WORLD, &status );
+            printf("Process %d is Repassing data %d\n", my_rank, fuck);
+            MPI_Send( &fuck, 1, MPI_INT, (my_rank+1)%proc_n, tag, MPI_COMM_WORLD);
+        }
     }
     MPI_Finalize();
 }
